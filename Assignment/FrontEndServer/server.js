2cee075a0649eea8c10b78a383465c41b564401e
Keefe <chenlinlikeefe@gmail.com>
2026-07-05 16:00:12 +0800

    New change

--- Assignment/FrontEndServer/server.js
+++ Assignment/FrontEndServer/server.js
@@ -1,36 +1,75 @@
 /*
-Summary: The server.js is used to start the  frontend server and website.
+Summary: The server.js is used to start the frontend server with HTTPS.
 */
 
-//const app = require('./controller/app');
-const express=require('express');
-const serveStatic=require('serve-static');
+const express = require('express');
+const serveStatic = require('serve-static');
+const https = require('https');
+const fs = require('fs');
 
-var hostname="localhost";
-var port=3001;
+var hostname = "localhost";
+var port = 3001;
 
-var app=express();
 
+var app = express();
+
+
+// Only allow GET requests
 app.use(function(req,res,next){
+
     console.log(req.url);
     console.log(req.method);
     console.log(req.path);
     console.log(req.query.id);
 
-    if(req.method!="GET"){
+
+    if(req.method != "GET"){
+
         res.type('.html');
-        var msg="<html><body>This server only serves web pages with GET!</body></html>";
+
+        var msg =
+        "<html><body>This server only serves web pages with GET!</body></html>";
+
         res.end(msg);
+
     }else{
+
         next();
+
     }
+
 });
 
 
-app.use(serveStatic(__dirname+"/public"));
+// Serve frontend files
+app.use(
+    serveStatic(__dirname + "/public")
+);
 
 
-app.listen(port,hostname,function(){
+// TLS certificate
+var options = {
 
-    console.log(`Server hosted at http://${hostname}:${port}`);
+    key: fs.readFileSync(
+        './certs/localhost-key.pem'
+    ),
+
+    cert: fs.readFileSync(
+        './certs/localhost.pem'
+    )
+
+};
+
+
+// HTTPS frontend server
+https.createServer(
+    options,
+    app
+)
+.listen(port, hostname, function(){
+
+    console.log(
+    `Frontend hosted at https://${hostname}:${port}`
+    );
+
 });
